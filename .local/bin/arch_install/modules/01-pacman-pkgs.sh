9952699da333bafa267bbc43a36725b63f13a5e5
leopoldguyot <leopold.guyot13@gmail.com>
2025-11-26 11:15:17 +0100

    update pacman list name

--- .local/bin/arch_install/modules/01-pacman-pkgs.sh
+++ .local/bin/arch_install/modules/01-pacman-pkgs.sh
@@ -4,7 +4,7 @@ set -e
 echo "[Module] Installing pacman packages"
 
 # Install official repo packages
-pacman_packages=$(grep -v '^#' pacman-packages.txt | tr '\n' ' ')
+pacman_packages=$(grep -v '^#' pacman-pkgs-list.txt | tr '\n' ' ')
 if [[ -n "$pacman_packages" ]]; then
     echo "[+] Installing pacman packages"
     sudo pacman --noconfirm --needed -S $pacman_packages
