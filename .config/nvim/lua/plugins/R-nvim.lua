489b2005eebcc046a54fd4e5857718831d04e3ed
leopoldguyot <leopold.guyot13@gmail.com>
2026-04-02 16:59:59 +0200

    fix startup

--- .config/nvim/lua/plugins/R-nvim.lua
+++ .config/nvim/lua/plugins/R-nvim.lua
@@ -14,12 +14,18 @@ return {
                 return
             end
 
-            local expr = string.format(
-                [=[if(requireNamespace("httpgd",quietly=TRUE)){d<-grDevices::dev.list();if(is.null(d)||!any(names(d)=="httpgd"))try(httpgd::hgd(host="127.0.0.1",port=%d,token=FALSE,silent=TRUE),silent=TRUE);.C(nvimcom_msg_to_nvim,"require('r.log').warn('httpgd URL: http://127.0.0.1:%d')")}else .C(nvimcom_msg_to_nvim,"require('r.log').warn(\"Install httpgd once with: install.packages('httpgd')\")")]=],
-                httpgd_port,
-                httpgd_port
-            )
-            require('r.run').send_to_nvimcom("E", expr)
+            require('r.send').cmd(string.format([=[
+if (requireNamespace("httpgd", quietly = TRUE)) {
+  devs <- grDevices::dev.list()
+  if (is.null(devs) || !any(names(devs) == "httpgd")) {
+    httpgd::hgd(host = "127.0.0.1", port = %d, token = TRUE, silent = TRUE)
+  }
+  url <- httpgd::hgd_url(host = "127.0.0.1", port = %d)
+  cat("\nhttpgd URL: ", url, "\n\n", sep = "")
+} else {
+  cat("\nInstall httpgd once with: install.packages('httpgd')\n\n")
+}
+]=], httpgd_port, httpgd_port))
         end
 
         require('r').setup({
